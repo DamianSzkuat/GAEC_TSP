9b81c2bad8723609cc773b625bf648edb6692bcb
YUyttendaele <45822639+YUyttendaele@users.noreply.github.com>
2018-12-16 20:21:20 +0100

    Add files via upload

--- template/Heuristic.m
+++ template/Heuristic.m
@@ -1,17 +1,32 @@
-function [Betterpath] = Heuristic(X, Y, Path)
+function [Betterpath] = Heuristic(X, Y, Path,START,SNIPPETLENGTH,small)
+
 % INPUT= x,y coordinates, path representation, OUTPUT = pathrepresentation.
 %HEURISTIC
 %   This heuristic reduces the cost of a path by removing the crossing (if any)
 %   with the largest cost attached to it.
+SMALL =7;
 OriginalPath = Path;
+% We orden the coordinates according to the given path.
+Xpath = X(Path); Ypath= Y(Path);
 % Number of Cities
 Nvar =length(X);
-% We orden the coordinates according to the given path.
+Pathsnippet=0;
+% instantiate possible betterpath
+Betterpath =Path;
+% extract the part of the polygon
+if ( START + 1+ SNIPPETLENGTH <= Nvar)
+    Pathsnippet = Path(START+1:START+1 + SNIPPETLENGTH);
+    XPathsnippet = Xpath(START+1:START+1 + SNIPPETLENGTH);
+    YPathsnippet = Ypath(START+1:START+1 + SNIPPETLENGTH);
+else
+    Pathsnippet =[Path(START+1: Nvar), Path(1: (SNIPPETLENGTH-Nvar+START))];
+    XPathsnippet =[Xpath(START+1: Nvar); Xpath(1: (SNIPPETLENGTH-Nvar+START))];
+    YPathsnippet =[Ypath(START+1: Nvar); Ypath(1: (SNIPPETLENGTH-Nvar+START))];
+end    
 
-Xpath = X(Path); Ypath= Y(Path);
 
 % Find the intersecting segments.
-[~,~, Segments]=selfintersect(Xpath,Ypath); % returns intersecting segments in path order.
+[~,~, Segments]=selfintersect(XPathsnippet,YPathsnippet); % returns intersecting segments in path order.
 
 if( size(Segments,1) >0)
     % Initialize list of paths with a crossing undone
@@ -41,7 +56,7 @@ if( size(Segments,1) >0)
     for i = 1: size(Segments,1)
         A= Segments(i,1); C= Segments(i,2);
          % find the order of appearence in the path.
-        if ( A == Nvar)
+        if ( A == SNIPPETLENGTH)
             B=1;
         else 
             B= A+1;
@@ -56,24 +71,34 @@ if( size(Segments,1) >0)
         % Start,....A,B....C,D. Where AB crosses with CD. A is connected to D so we connect A to C now.
 
         %connect A to C.
-        Order= [1: Nvar];
+        Order= [1: SNIPPETLENGTH];
         temp= Order(B);        
         Order(B)= Order(C);
         Order(C)= temp;
         
-        Uncross(i,:) = Path(Order);
+        %uncrossed snippet
+        UncrossedSnippet = Pathsnippet(Order);
+             
+        if ( START + SNIPPETLENGTH +1 <= Nvar)
+            Uncross(i,:) = [Path(1:START),UncrossedSnippet,Path((START+1 + SNIPPETLENGTH): Nvar)]; 
+        else 
+            Uncross(i,:) = [UncrossedSnippet(Nvar-START +1 : SNIPPETLENGTH),Path(SNIPPETLENGTH-(Nvar-START)+1:START),UncrossedSnippet(1 : Nvar-START)];     
+        end
 
-
-        %evaluate objective function
-
-        R  = path2adj(Uncross(i,:));
-        Objval = Dist(R(1),1);
-        for t= 2:length(R)
-            Objval= Objval + Dist(R(t),t);
-        end   
-        CostReduction(i) = OriginalCost-Objval; 
-
+        %evaluate objective function of the whole graph, only deemed necessary when the snippetlength is larger than SMALL.
+        if( SNIPPETLENGTH>SMALL)
+            R  = path2adj(Uncross(i,:));
+            Objval = Dist(R(1),1);
+            for t= 2:length(R)
+                Objval= Objval + Dist(R(t),t);
+            end   
+            CostReduction(i) = OriginalCost-Objval; 
+        
+        else    
+            Betterpath = Uncross(i,:);
+        end        
     end 
+if(SNIPPETLENGTH>SMALL)     
     [~, Pos]= max(CostReduction);
     % pick the path with the greatest cost reduction.
     if ( max(CostReduction) >0 )
@@ -81,7 +106,6 @@ if( size(Segments,1) >0)
     else 
         Betterpath = OriginalPath;
     end    
-else 
-    Betterpath = OriginalPath;
 end
+%end of function    
 end
--- template/tsp_ImprovePopulation.m
+++ template/tsp_ImprovePopulation.m
@@ -16,27 +16,66 @@
 % Output parameter:
 %   improvedPopulation  - the new population after loop removal (if improve
 %                          <> 0, else the unchanged population).
-
-function newpop = tsp_ImprovePopulation(popsize, ncities, pop, improve, dists, REPRESENTATION, x, y, HEURISTIC)
+function newpop = tsp_ImprovePopulation(popsize,pop,improve,dists,x,y)
 
 if (improve)
-   for i=1:popsize
+     for i=1:popsize
      if isequal(REPRESENTATION,'adj')
-        result = improve_path(ncities, adj2path(pop(i,:)),dists);
-        if isequal(HEURISTIC,'cross_elimination')
-            result = Heuristic(x,y,result);
+         result =   adj2path(pop(i,:));
+         if isequal(HEURISTIC,'cross_elimination')     
+            % untangle small crosses
+            start = randi([1,ncities]);
+            SMALL=7;
+            for j= 0: ceil (ncities/SMALL) 
+                result=Heuristic(x, y, result ,mod(start +SMALL*j,ncities),SMALL);
+            end      
+           %untangle large crosses 
+           LARGE= 40;
+           sniplength2=0;
+           if( ncities >= LARGE) 
+             sniplength2=randi([SMALL,LARGE]);
+           else
+             sniplength2= randi([SMALL,ncities]);
+           end
+           SampleAmount = ceil (ncities/LARGE); 
+           for k= 0: SampleAmount
+            start2 = randi([1,ncities]);
+            result=Heuristic(x, y,result ,start2,sniplength2);     
+           end
         end
-        pop(i,:) = path2adj(result);
-     elseif isequal(REPRESENTATION,'path')
-        result = improve_path(ncities, pop(i,:),dists);
-        if isequal(HEURISTIC,'cross_elimination');
-            result = Heuristic(x,y,result)
-        end
-        pop(i,:) = result;
-     else
-        error('Representation not implemented!'); 
-    end 
-   end
+        % remove loops
+        result = improve_path(ncities,result ,dists);       
+        % convert to adjacency representation
+        pop(i,:) = path2adj(result); 
+      elseif isequal(REPRESENTATION,'path')
+            if isequal(HEURISTIC,'cross_elimination')
+                result = pop(i,:);
+                % untangle small crosses
+                start = randi([1,ncities]);
+                SMALL=7;
+                for j= 0: ceil (ncities/SMALL) 
+                    result=Heuristic(x, y,result ,mod(start +SMALL*j,ncities),SMALL);
+                end 
+                %untangle large crosses 
+                LARGE= 40;
+                sniplength2=0;
+                if( ncities >= LARGE) 
+                    sniplength2=randi([SMALL,LARGE]);
+                else
+                sniplength2= randi([SMALL,ncities]);             
+                end
+                SampleAmount = ceil (ncities/LARGE); 
+                for k= 0: SampleAmount;
+                    start2 = randi([1,ncities]);
+                    result=Heuristic(x, y,result ,start2,sniplength2);     
+                end
+            end  
+            % remove loops
+            pop(i,:)= improve_path(ncities,result ,dists);  
+      else
+            error('Representation not implemented!'); 
+      end        
+  end  
+  newpop = pop;    
 end
 
-newpop = pop;
